2e4b7eee8bc336e10ee470886067cd1470e9adae
kin280 <74567462+kin280@users.noreply.github.com>
2024-01-19 20:49:32 +0700

    Add project files.

---
+++ interfaceIColorable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public interface IColorable
+{
+    void HowToColor();
+}
+
+
+public class HinhVuong : IColorable
+{
+    public double Canh { get; set; }
+
+    public HinhVuong(double canh)
+    {
+        Canh = canh;
+    }
+
+    public void HowToColor()
+    {
+        Console.WriteLine("Tô màu cho hình vuông");
+    }
+}
+
+
+public class ChuongTrinh
+{
+    public static void Main()
+    {
+        HinhVuong hinhVuong = new HinhVuong(4);
+
+        Console.WriteLine("Hướng dẫn cách tô màu cho hình vuông:");
+        hinhVuong.HowToColor();
+    }
+}
